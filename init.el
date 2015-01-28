3492b2462f0d84446ca07f8b69c0b2d9a59c45e1
Nathan Kot <me@nathankot.com>
2015-01-28 18:51:10 +1300

    Language packs comment

--- init.el
+++ init.el
@@ -213,12 +213,14 @@
                   (flycheck-select-checker 'jsxhint-checker)
                   (flycheck-mode)))))))
 
+;; LANGUAGE PACKS
+;; ================================================================================
+
 (use-package js2-mode
   :commands js2-mode
   :init (progn
           (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
           (add-to-list 'interpreter-mode-alist '("node" . js2-mode))))
-
 
 (use-package web-mode
   :commands web-mode
