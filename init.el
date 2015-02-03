8c89b625aa6f3c4200a999ff443218e2e8edbf38
Nathan Kot <me@nathankot.com>
2015-02-03 14:32:53 +1300

    Add binding for helm buffer list.

--- init.el
+++ init.el
@@ -214,6 +214,7 @@
             (other-window 1)
             (switch-to-buffer buf))))
   :config (progn
+            (define-key helm-map (kbd "C-b") 'helm-buffer-list)
             (define-key helm-map (kbd "ESC") 'helm-keyboard-quit)
             (define-key helm-map (kbd "C-p") 'helm-keyboard-quit)
             (define-key helm-map (kbd "C-j") 'helm-next-line)
