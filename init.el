3ee21a58baeafcb8558f69266a4362d1be5ce40e
Nathan Kot <me@nathankot.com>
2015-02-19 10:16:44 +1300

    Workaround for company mode and fci.

--- init.el
+++ init.el
@@ -178,6 +178,23 @@
   :init (progn
           (setq company-dabbrev-downcase nil))
   :config (progn
+
+            ; There is a bug with company mode and FCI, this is a workaround
+            (defvar-local company-fci-mode-on-p nil)
+
+            (defun company-turn-off-fci (&rest ignore)
+              (when (boundp 'fci-mode)
+                (setq company-fci-mode-on-p fci-mode)
+                (when fci-mode (fci-mode -1))))
+
+            (defun company-maybe-turn-on-fci (&rest ignore)
+              (when company-fci-mode-on-p (fci-mode 1)))
+
+            (add-hook 'company-completion-started-hook 'company-turn-off-fci)
+            (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+            (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+            ; ---
+
             ; Add custom backends
             (add-to-list 'company-backends 'company-tern)
             ; Swap some keybindings
