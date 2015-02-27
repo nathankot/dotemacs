b5d67049cb89419f8d2087407e7b5a7beb0f9fc6
Nathan Kot <me@nathankot.com>
2015-02-27 13:14:57 +1300

    Dont copy from osx

--- init.el
+++ init.el
@@ -57,15 +57,12 @@
 (push "/usr/local/bin" exec-path)
 (setenv "PATH" (concat (getenv "PATH") ":/opt/boxen/homebrew/bin"))
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
 (defun paste-to-osx (text &optional push)
   (let ((process-connection-type nil))
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
       (process-send-string proc text)
       (process-send-eof proc))))
 (setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
 
 ;; Other stuff
 (setq inhibit-startup-screen t)
