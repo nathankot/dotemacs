f94f9b9e418a19112c95d78af519a57322fcaa5b
Nathan Kot <me@nathankot.com>
2015-08-26 17:06:02 +0900

    Make tag files web-mode

--- init.el
+++ init.el
@@ -552,6 +552,7 @@
           (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
           (add-to-list 'auto-mode-alist '("\\.liquid\\'" . liquid-mode))
           (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+          (add-to-list 'auto-mode-alist '("\\.tag\\'" . web-mode))
           (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
           (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
           (add-to-list 'auto-mode-alist '("\\.html.twig\\'" . web-mode))
