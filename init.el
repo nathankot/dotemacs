9a623e46286ac5d7329764f5d811d853d362696c
Nathan Kot <me@nathankot.com>
2015-08-26 23:02:29 +0900

    Install scss mode

--- init.el
+++ init.el
@@ -605,6 +605,12 @@
   :init (progn
           (add-to-list 'auto-mode-alist '("\\.stylus\\'" . stylus-mode))))
 
+(use-package scss-mode
+  :ensure t
+  :init (progn
+          (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+          (setq scss-compile-at-save nil)))
+
 (use-package php-mode
   :ensure t
   :commands php-mode)
