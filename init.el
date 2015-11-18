6345f8ad4664e98d8e8c2b45f1c60d014b6f8891
Nathan Kot <me@nathankot.com>
2015-11-19 02:04:05 +0900

    Add stackexchange mode

--- init.el
+++ init.el
@@ -791,3 +791,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; Stack Exchange
+;; ================================================================================
+(use-package sx
+  :ensure t)
