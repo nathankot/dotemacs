016d5e5108a1d8f3162e35f792ab1635c43d8e58
Nathan Kot <nkot@plaid.com>
2021-10-13 15:37:40 +1300

    Update safe local variables

--- init.el
+++ init.el
@@ -9,9 +9,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(custom-safe-themes
-     (quote
-       ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+ '(custom-safe-themes
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
+ '(safe-local-variable-values
+   '((eval progn
+           (magit-disable-section-inserter 'magit-insert-tags-header)
+           (magit-disable-section-inserter 'magit-insert-unpushed-to-upstream-or-recent)
+           (magit-disable-section-inserter 'magit-insert-unpulled-from-upstream)
+           (magit-disable-section-inserter 'magit-insert-stashes)
+           (magit-disable-section-inserter 'magit-insert-unpulled-from-upstream)
+           (magit-disable-section-inserter 'magit-insert-untracked-files)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
