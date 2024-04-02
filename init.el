e61b1cc5fb25922b994c1d9d6b3a6188f9407f12
Nathan Kot <me@nathankot.com>
2024-04-03 01:05:22 +0900

    lsp related tweaks

--- init.el
+++ init.el
@@ -12,8 +12,10 @@
  '(custom-safe-themes
     '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" "434f947950121d38d6246a4712014447a367a8e1b3a8c818c0b70e9a464147e7" "e70e87ad139f94d3ec5fdf782c978450fc2cb714d696e520b176ff797b97b8d2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(safe-local-variable-values
-    '((lsp-rust-analyzer-rustfmt-extra-args .
-        [])
+    '((lsp-rust-analyzer-checkonsave-features .
+        ["ssr"])
+       (lsp-rust-analyzer-rustfmt-extra-args .
+         [])
        (lsp-rust-analyzer-rustfmt-override-command .
          ["leptosfmt"
            (\, "--stdin")
@@ -1020,16 +1022,17 @@ Otherwise deletes a character normally by calling `backward-delete-char'."
   (lsp-inlay-hint-enable t)
   (lsp-response-timeout 10)
   (lsp-idle-delay 3)
-  (lsp-prefer-flymake :none)
   (lsp-print-performance nil)
   (lsp-auto-guess-root t)
   (lsp-enable-file-watchers nil)
-  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-enable-folding nil)
+  (lsp-enable-suggest-server-download nil)
   (lsp-enable-symbol-highlighting t)
+  (lsp-headerline-breadcrumb-enable nil)
   (lsp-ui-doc-enable nil)
   (lsp-lens-enable nil)
   (lsp-ui-sideline-enable nil)
-  (lsp-diagnostics-provider :none)
+  (lsp-diagnostics-provider :flycheck)
   (lsp-signature-render-documentation t)
   (lsp-signature-auto-activate t)
   (lsp-eldoc-enable-hover t)
@@ -1037,32 +1040,7 @@ Otherwise deletes a character normally by calling `backward-delete-char'."
   ;; (lsp-log-io t "enable for debugging")
   (lsp-log-max nil "disable logging")
 
-  ;; Language-specific settings:
-
-  ;; rust
-  (lsp-rust-server 'rust-analyzer)
-  (lsp-go-hover-kind "NoDocumentation")
-  (lsp-rust-analyzer-rustfmt-extra-args ["+nightly"])
-
-  ;; golang
-  ;; (lsp-go-gopls-server-args '("--debug=localhost:6060"))
-  (lsp-go-gopls-server-args '("-remote=auto"))
-  (lsp-go-codelenses nil)
-  (lsp-go-symbol-matcher "CaseInsensitive")
-  (lsp-go-directory-filters [
-    "-node_modules"
-    "-bin"
-    "-docs"
-    "-node"
-    "-proto"
-    "-resources"
-    "-tmp"
-  ])
-
-  :init
-
   :config
-
   (evil-leader/set-key "jt" 'lsp-goto-type-definition)
   (evil-leader/set-key "ji" 'lsp-goto-implementation)
   (evil-leader/set-key "jd" 'lsp-find-definition)
@@ -1316,15 +1294,20 @@ Otherwise deletes a character normally by calling `backward-delete-char'."
   :straight t
   :mode "\\.kt\\'")
 
-(use-package rust-mode
+(use-package rustic
   :straight t
   :mode ("\\.rs\\'" . poly-rust+web-mode)
   :custom
-  ;; (lsp-rust-analyzer-display-reborrow-hints "always")
+  (rustic-lsp-setup-p  nil)
+  (lsp-rust-server 'rust-analyzer)
+  (lsp-rust-analyzer-rustfmt-extra-args ["+nightly"])
+  (lsp-rust-analyzer-cargo-watch-enable t)
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
   (lsp-rust-analyzer-display-parameter-hints t)
   (lsp-rust-analyzer-display-lifetime-elision-hints-enable "always")
-  (lsp-rust-analyzer-diagnostics-enable nil)
+  (lsp-rust-analyzer-diagnostics-enable t)
   (lsp-rust-analyzer-max-inlay-hint-length 12)
+  ;; (lsp-rust-analyzer-display-reborrow-hints "always")
   ;; (lsp-rust-analyzer-server-command '("rust-analyzer" "+nightly"))
 
   :hook
@@ -1449,14 +1432,28 @@ INITIAL will be used as the initial input, if given."
   :mode "\\.go\\'"
   :hook (go-mode . lsp-deferred)
   :hook (before-save . gofmt-before-save)
+  :custom
+  (lsp-go-hover-kind "NoDocumentation")
+  ;; (lsp-go-gopls-server-args '("--debug=localhost:6060"))
+  (lsp-go-gopls-server-args '("-remote=auto"))
+  (lsp-go-codelenses nil)
+  (lsp-go-symbol-matcher "CaseInsensitive")
+  (lsp-go-directory-filters [
+    "-node_modules"
+    "-bin"
+    "-docs"
+    "-node"
+    "-proto"
+    "-resources"
+    "-tmp"
+  ])
+  (gofmt-command "goimports")
   :init
   (use-package gotest
     :straight t
     :config
     (evil-leader/set-key-for-mode 'go-mode "tc" 'go-test-current-test)
-    (evil-leader/set-key-for-mode 'go-mode "tf" 'go-test-current-file))
-  :config
-  (setq gofmt-command "goimports"))
+    (evil-leader/set-key-for-mode 'go-mode "tf" 'go-test-current-file)))
 
 (use-package c-mode
   :hook ((c-mode c++-mode) . #'lsp-deferred)
